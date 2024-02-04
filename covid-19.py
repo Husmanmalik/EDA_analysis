69f1a20d664b113c52016bb00b50f91d6922af23
Hafiz Muhammad Usman <141828340+Husmanmalik@users.noreply.github.com>
2024-02-05 02:36:03 +0500

    Update covid-19.py

--- covid-19.py
+++ covid-19.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
 import pandas as pd
 from pandas_profiling import ProfileReport
-df=pd.read_csv('PK COVID-19-10may.csv')
+df=pd.read_csv('data_set1.csv')
 df1 = pd.DataFrame(df)
 # Generate a profile report
 st.title('EDA Analysis of Covid-19 Cases')
